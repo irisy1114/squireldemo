6dd8ca1fd2646e2eb912b840d2ced3b716af189e
Iris Yao <91230673+irisy1114@users.noreply.github.com>
2023-02-28 20:06:30 -0600

    add sighting list page

--- src/main/java/edu/wctc/squireldemo/SightingController.java
+++ src/main/java/edu/wctc/squireldemo/SightingController.java
@@ -1,5 +1,7 @@
 package edu.wctc.squireldemo;
 
+import edu.wctc.squireldemo.entity.Sighting;
+import edu.wctc.squireldemo.entity.Squirrel;
 import edu.wctc.squireldemo.service.LocationService;
 import edu.wctc.squireldemo.service.SightingService;
 import edu.wctc.squireldemo.service.SquirrelService;
@@ -8,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class SightingController {
@@ -39,7 +44,21 @@ public class SightingController {
     }
 
     @GetMapping("/sightings")
-    public String showSightingsList() {
+    public String showSightingsList(@RequestParam("id") String idString, Model model) {
+        int squirrelId = 0;
+        try {
+            squirrelId = Integer.parseInt(idString);
+        } catch (NumberFormatException e) {
+            squirrelId = 1;
+        }
+        // get squirrel with matching ID and add to model
+        Squirrel squirrel = squirrelService.getSquirrel(squirrelId);
+        model.addAttribute("commonName", squirrel.getCommonName());
+
+        // get sightings with matching squirrel ID and add to model
+        List<Sighting> list = sightingService.getSightingsForSquirrel(squirrelId);
+        model.addAttribute("sightingList", list);
+
         return "sighting-list";
     }
 
